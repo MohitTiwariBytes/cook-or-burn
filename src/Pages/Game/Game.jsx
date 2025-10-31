90a763f68e6b0cc036fd2ea32f8ab9175ed83bf4
MohitTiwariBytes <mohittiwaridev@gmail.com>
2025-10-31 16:42:06 +0530

    helo

--- src/Pages/Game/Game.jsx
+++ src/Pages/Game/Game.jsx
@@ -1,13 +1,41 @@
 import React from "react";
-import "./Game.css"
+import "./Game.css";
 import gsap from "gsap";
+import rageBait from "../../assets/ragebaitGif.gif";
 
-export default function Game(){
-    return(
-        <div className="main-game-page">
-            <div className="gamePage">
-                <h1>hi fgame</h1>
+export default function Game() {
+  return (
+    <>
+      <div className="notice2">
+        <h1>sorry your device is a bit too small for this 💔</h1>
+      </div>
+      <div className="main-game-page">
+        <div className="gamePage">
+          <div className="top-nav">
+            <h1>Cook or burn? </h1>
+          </div>
+          <div className="gifs-overlay">
+            <img id="gif-1" src={rageBait} alt="" />
+            <img id="gif-2" src={rageBait} alt="" />
+            <img id="gif-3" src={rageBait} alt="" />
+          </div>
+          <div className="game-main">
+            <div className="txt-title">
+              <span>Enter your favorite food combo!</span>
             </div>
+            <div className="inputs">
+              <input type="text" />
+              <span>+</span>
+              <input type="text" />
+            </div>
+            <div className="submit-button">
+              <button style={{ width: "427px" }} id="btn">
+                Roast me please!
+              </button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </>
+  );
 }
